b81a11d61acae551140e2bcd779f5508f7def44a
Hafiz Shamnad <99228865+Hafiz-shamnad@users.noreply.github.com>
2024-02-13 16:29:36 +0530

    Update FrequencyWithVowels.java

--- FrequencyWithVowels.java
+++ FrequencyWithVowels.java
@@ -4,7 +4,7 @@ public class FrequencyWithVowels {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter a word:");
         String word = sc.nextLine();
-        int vowels = 0;
+        int vowels = 0 , consonants = 0;
         System.out.println("Character frequencies:");
         for(char ch = 'a' ; ch <= 'z' ; ch++){
             int count = 0 ;
@@ -21,9 +21,10 @@ public class FrequencyWithVowels {
         for(int i = 0 ; i < word.length() ; i++) {
             if (word.charAt(i) == 'a' || word.charAt(i) == 'e' || word.charAt(i) == 'i' || word.charAt(i) == 'o' || word.charAt(i) == 'u') {
                 vowels++;
+            }else{
+                consonants++;
             }
         }
-        int consonants = word.length()-vowels;
         System.out.println("Number of consonants = " + consonants);
         System.out.println("Number of vowels = " + vowels);
     }
